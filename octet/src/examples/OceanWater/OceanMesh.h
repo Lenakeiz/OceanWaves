1729d772dca7a0b72ccd62c8a892594fcef16e87
Andrea Castegnaro <andrea.castegnaro@outlook.com>
2015-03-30 17:07:57 +0100

    Added basic plan, started to add Tessendorf fft

--- octet/src/examples/OceanWater/OceanMesh.h
+++ octet/src/examples/OceanWater/OceanMesh.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define SEG_WIDTH 10.0f
-#define MULTIPLIER 5
+#define SEG_WIDTH 1.0f
+#define MULTIPLIER 1
 
 namespace octet
 {
@@ -23,10 +23,10 @@ namespace octet
 			//vec3p origin;
 		};
 
-		VertexOcean* vertices;
-		unsigned int vertCount;
-		unsigned int* indices;
-		unsigned int indexCount;
+		dynarray<VertexOcean> vertices;
+		uint32_t vertCount;
+		dynarray<uint32_t> indices;
+		uint32_t indexCount;
 
 		ref<mesh> oceanMesh;
 
@@ -49,6 +49,15 @@ namespace octet
 
 		OceanMesh() : _speed(4.0f), _L_Length(2.0f), _amplitude(1.0f)
 		{
+			oceanMesh = new mesh();
+		}
+
+		~OceanMesh()
+		{
+		}
+
+		void Init()
+		{
 			unsigned int map_widht = _m * MULTIPLIER;
 			unsigned int map_height = _n * MULTIPLIER;
 
@@ -57,17 +66,15 @@ namespace octet
 
 			float half_terrain_width = terrain_width * 0.5f;
 			float half_terrain_height = terrain_eight * 0.5f;
-
-			oceanMesh = new mesh();
-
-			vertices = new VertexOcean[map_widht * map_height];
+			
+			//vertices = new VertexOcean[map_widht * map_height];
 
 			for (unsigned int j = 0; j < map_height; ++j)
 			{
 				for (unsigned int i = 0; i < map_widht; ++i)
 				{
-					unsigned int idx = i + (j * map_widht);
-					
+					//unsigned int idx = i + (j * map_widht);
+
 					float u = i / static_cast<float>(map_widht - 1);
 					float v = j / static_cast<float>(map_height - 1);
 
@@ -75,61 +82,91 @@ namespace octet
 					float y = 0.0f;
 					float z = (v * terrain_eight) - half_terrain_height;
 
-					vertices[idx].norm = vec3p(0.0f, 1.0f, 0.0f);
-					vertices[idx].pos = vec3p(x, y, z);
+					VertexOcean vo;
+					vo.norm = vec3p(0.0f, 1.0f, 0.0f);
+					vo.pos = vec3p(x, y, z);
+
+					vertices.push_back(vo);
+
+					//vertices[idx].norm = vec3p(0.0f, 1.0f, 0.0f);
+					//vertices[idx].pos = vec3p(x, y, z);
 					++vertCount;
 				}
+
 			}
 
 			// Generate indices.
 			const unsigned int num_triangles = (map_widht - 1) * (map_height - 1) * 2;
-			indices = new unsigned int[num_triangles * 3];
+			//indices = new unsigned int[num_triangles * 3];
 
-			unsigned int index = 0;
+			//unsigned int index = 0;
 			for (unsigned int j = 0; j < map_height - 1; ++j)
 			{
 				for (unsigned int i = 0; i < map_widht - 1; ++i)
 				{
 					unsigned int vertex_index = i + (j * map_widht);
-					indices[index++] = vertex_index;
-					indices[index++] = vertex_index + map_widht;
-					indices[index++] = vertex_index + 1;
 
-					indices[index++] = vertex_index + map_widht;
-					indices[index++] = vertex_index + map_widht + 1;
-					indices[index++] = vertex_index + 1;
+					indices.push_back(vertex_index);
+					indices.push_back(vertex_index + map_widht);
+					indices.push_back(vertex_index + 1);
+
+					indices.push_back(vertex_index + map_widht);
+					indices.push_back(vertex_index + map_widht + 1);
+					indices.push_back(vertex_index + 1);
 				}
 			}
-			indexCount = index - 1;
-		
+
+			//indexCount = index - 1;
+
 			SetupRendering();
-
 		}
 
-		~OceanMesh()
+		void Update(float deltaTime)
 		{
+			SimulateOceanFFT(_simulationTime, 1.0f / (MULTIPLIER * SEG_WIDTH));
+			//SetupRendering();
+			_simulationTime += deltaTime;
 		}
-
-		void Init(){}
-
-		void Update(){}
 
 		void Draw(){}
+
+		mesh* getMeshInstance()
+		{
+			return oceanMesh;
+		}
 
 	private:
 		void SetupRendering()
 		{
-			oceanMesh->allocate(sizeof(VertexOcean) * vertCount, sizeof(uint32_t) * indexCount);
-			oceanMesh->set_params(sizeof(VertexOcean), indexCount, vertCount, GL_TRIANGLES, GL_UNSIGNED_INT);
+			//oceanMesh->allocate(sizeof(VertexOcean) * vertCount, sizeof(uint32_t) * indexCount);
+			//oceanMesh->set_params(sizeof(VertexOcean), indexCount, vertCount, GL_TRIANGLES, GL_UNSIGNED_INT);
 
-			// describe the structure of my_vertex to OpenGL
+			//oceanMesh->allocate(sizeof(vert)*plane_.size(), sizeof(unsigned)*indicies.size());
+			//oceanMesh->set_params(sizeof(vert), indicies.size(), plane_.size(), GL_TRIANGLES, GL_UNSIGNED_INT);
+			//oceanMesh->clear_attributes();
+			//oceanMesh->add_attribute(octet::attribute_pos, 3, GL_FLOAT, 0);
+			//oceanMesh->add_attribute(octet::attribute_normal, 3, GL_FLOAT, 12, 0);
+			//oceanMesh->add_attribute(octet::attribute_color, 4, GL_UNSIGNED_BYTE, 24, TRUE);
+
+			//octet::gl_resource::wolock vl(mesh_->get_vertices());
+			//octet::gl_resource::wolock il(mesh_->get_indices());
+			//unsigned * indxP = (unsigned*)il.u8();
+			//vert *vtxP = (vert*)vl.u8();
+
+			//// describe the structure of my_vertex to OpenGL
+			oceanMesh->clear_attributes();
 			oceanMesh->add_attribute(attribute_pos, 3, GL_FLOAT, 0);
 			oceanMesh->add_attribute(attribute_normal, 3, GL_FLOAT, 12);
 			oceanMesh->add_attribute(attribute_uv, 2, GL_FLOAT, 24);
 			oceanMesh->set_params(32, 0, 0, GL_TRIANGLES, GL_UNSIGNED_INT);
 		
+			oceanMesh->set_vertices(vertices);
+			oceanMesh->set_indices(indices);
 			
 		}
+
+		void SimulateOceanFFT
+
 	};
 }
 
--- octet/src/examples/OceanWater/OceanWater.h
+++ octet/src/examples/OceanWater/OceanWater.h
@@ -12,28 +12,49 @@ namespace octet {
   class OceanWater : public app {
     // scene for drawing box
     ref<visual_scene> app_scene;
+	mouse_look mouse_look_helper;
+
+	ref<camera_instance> camera;
+
+	OceanMesh* om;
   public:
     /// this is called when we construct the class before everything is initialised.
     OceanWater(int argc, char **argv) : app(argc, argv) {
     }
 
     /// this is called once OpenGL is initialized
-    void app_init() {
-      app_scene =  new visual_scene();
-      app_scene->create_default_camera_and_lights();
+	void app_init() {
+		mouse_look_helper.init(this, 200.0f / 360.0f, false);
+		om = new OceanMesh();
+		om->Init();
+		app_scene =  new visual_scene();
+		app_scene->create_default_camera_and_lights();
 
-      material *red = new material(vec4(1, 0, 0, 1));
-      mesh_box *box = new mesh_box(vec3(4));
-      scene_node *node = new scene_node();
-      app_scene->add_child(node);
-      app_scene->add_mesh_instance(new mesh_instance(node, box, red));
-    }
+		camera = app_scene->get_camera_instance(0);
+		camera->get_node()->translate(vec3(0, 4, 0));
+		camera->set_far_plane(10000);
+
+		mat4t mat;
+
+		mat.loadIdentity();
+		mat.translate(0, -0.5f, 0);
+		material *color = new material(vec4(0, 0, 1, 0.5f));
+		scene_node *node = new scene_node();
+		
+		app_scene->add_child(node);
+		app_scene->add_mesh_instance(new mesh_instance(node, om->getMeshInstance(), color));
+
+	}
 
     /// this is called to draw the world
     void draw_world(int x, int y, int w, int h) {
       int vx = 0, vy = 0;
       get_viewport_size(vx, vy);
       app_scene->begin_render(vx, vy);
+
+	  scene_node *camera_node = camera->get_node();
+	  mat4t &camera_to_world = camera_node->access_nodeToParent();
+	  mouse_look_helper.update(camera_to_world);
 
       // update matrices. assume 30 fps.
       app_scene->update(1.0f/30);
@@ -43,8 +64,8 @@ namespace octet {
 
       // tumble the box  (there is only one mesh instance)
       scene_node *node = app_scene->get_mesh_instance(0)->get_node();
-      node->rotate(1, vec3(1, 0, 0));
-      node->rotate(1, vec3(0, 1, 0));
+      //node->rotate(1, vec3(1, 0, 0));
+      //node->rotate(1, vec3(0, 1, 0));
     }
   };
 }
